19374140e0174ab71e4d730f89b8b15c27fab360
vorontsov <mimictear@outlook.com>
2023-03-14 10:32:49 +0100

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by ANDREY VORONTSOV on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
